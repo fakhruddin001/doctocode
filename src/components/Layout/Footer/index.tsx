9a752e10d1aa8d89c4e86eeab912e7150062a86c
Fakhruddin <fakhruddin.n@bureauveritas.com>
2025-08-06 18:09:57 +0530

    fix(Footer): Update footer text and adjust padding for improved layout

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,16 +1,13 @@
 import React, { FC } from "react";
 import Link from "next/link";
-import { headerData } from "../Header/Navigation/menuData";
-import { Icon } from "@iconify/react";
-import Logo from "../Header/Logo";
 
 const Footer: FC = () => {
   return (
-    <footer className="pt-16 bg-darkmode">
+    <footer className=" bg-darkmode">
       <div className="container mx-auto lg:max-w-screen-xl md:max-w-screen-md px-4">
-        <div className="border-t border-grey/15 dark:border-white/15 py-10 flex justify-center items-center">
+        <div className="border-t border-grey/15 dark:border-white/15 py-2 flex justify-center items-center">
           <p className="text-sm text-black/70 dark:text-white/70">
-            Doc to Code. Owned By{" "}
+            Codient. Owned By{" "}
             <Link
               href="https://group.bureauveritas.com/"
               target="_blank"
